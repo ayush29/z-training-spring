f28d34b55c47d4bee424bc7131b47735b719099c
sai-kamal <amuluru.81@gmail.com>
2020-08-04 20:13:14 +0530

    added reviews backend without DB

---
+++ src/main/java/com/ayush/ztrainingspring/reviews/dao/ReviewDao.java
@@ -0,0 +1,23 @@
+package com.ayush.ztrainingspring.reviews.dao;
+
+import com.ayush.ztrainingspring.reviews.model.Comment;
+import com.ayush.ztrainingspring.reviews.model.Review;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.UUID;
+
+public interface ReviewDao {
+    int addReview(Review review);
+    List<Review> getAllReviews();
+
+    Comment addComment(UUID id, Comment text) throws MalformedURLException;
+
+    int addLike(UUID id);
+
+    List<Comment> getComments(UUID id);
+
+    List<Review> getAllSortedReviews(String option);
+
+    int getNumReviews();
+}
