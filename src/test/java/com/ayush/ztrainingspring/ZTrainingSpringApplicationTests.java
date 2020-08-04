68150286911f4d0ed8c883f36f09a1c5503abc25
ayush29 <ayushjaindl@gmail.com>
2020-08-04 18:02:01 +0530

    initialise

---
+++ src/test/java/com/ayush/ztrainingspring/ZTrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.ztrainingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZTrainingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
