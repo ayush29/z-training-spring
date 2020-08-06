5b2e3340979f88a03261fd6bc17714aec0563309
ayush29 <ayushjaindl@gmail.com>
2020-08-06 09:59:31 +0530

    cross origin for auth controller

--- src/main/java/com/ayush/ztrainingspring/user_auth/AuthController.java
+++ src/main/java/com/ayush/ztrainingspring/user_auth/AuthController.java
@@ -3,12 +3,14 @@ package com.ayush.ztrainingspring.user_auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:8081"})
 @RestController
 public class AuthController {
 
