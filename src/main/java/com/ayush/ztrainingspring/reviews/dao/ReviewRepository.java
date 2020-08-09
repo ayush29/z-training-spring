2dbbdcfe032bed7d34b98e8107890bf06e8c3a3f
sai-kamal <amuluru.81@gmail.com>
2020-08-10 00:17:49 +0530

    updated controller and service

--- src/main/java/com/ayush/ztrainingspring/reviews/dao/ReviewRepository.java
+++ src/main/java/com/ayush/ztrainingspring/reviews/dao/ReviewRepository.java
@@ -4,6 +4,12 @@ import com.ayush.ztrainingspring.reviews.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByOrderByCreatedTimeAsc();
+    List<Review> findByOrderByCreatedTimeDesc();
+    List<Review> findByOrderByRatingAscCreatedTimeDesc();
+    List<Review> findByOrderByRatingDescCreatedTimeDesc();
 }
--- src/main/java/com/ayush/ztrainingspring/reviews/model/Comment.java
+++ src/main/java/com/ayush/ztrainingspring/reviews/model/Comment.java
@@ -1,39 +1,46 @@
 package com.ayush.ztrainingspring.reviews.model;
 
 import com.ayush.ztrainingspring.user_auth.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import java.util.Calendar;
 
 @Entity
 @Table(name = "comments")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Lob
     @Column(nullable = false)
     private String text;
 
-    @OneToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private Calendar createdTime;
+
+    @JsonIgnore
+    @OneToOne(optional = false, fetch = FetchType.LAZY, cascade = CascadeType.ALL )
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
-    @ManyToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    @ManyToOne(optional = false, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "review_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Review review;
 
     public Comment() {}
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -51,5 +58,21 @@ public class Comment {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public Calendar getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Calendar createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
     }
 }
